9e06380ec92312080287402ed703e57d833d6908
Brual Shah <brual@atlassian.com>
2017-06-10 19:50:24 -0700

    updated primary grasph location

--- include/parts_grasp_loc.h
+++ include/parts_grasp_loc.h
@@ -7,28 +7,21 @@
 using namespace std;
 
 std::vector<tf::Vector3> disk_primary_loc;
-disk_primary_loc.push_back(tf::Vector3(-0.21, -0.21, 0));
-disk_primary_loc.push_back(tf::Vector3(0.21, -0.21, 0));
-disk_primary_loc.push_back(tf::Vector3(-0.21, 0.21, 0));
-disk_primary_loc.push_back(tf::Vector3(0.21, 0.21, 0));
-disk_primary_loc.push_back(tf::Vector3(-0.1251, -0.1251, 0));
-disk_primary_loc.push_back(tf::Vector3(0.1251, -0.1251, 0));
-disk_primary_loc.push_back(tf::Vector3(-0.1251, 0.1251, 0));
-disk_primary_loc.push_back(tf::Vector3(0.1251, 0.1251, 0));
+disk_primary_loc.push_back(tf::Vector3(-0.15, -0.15, 0));
+disk_primary_loc.push_back(tf::Vector3(-0.15, 0.15, 0));
+disk_primary_loc.push_back(tf::Vector3(0.15, -0.15, 0));
+disk_primary_loc.push_back(tf::Vector3(0.15, 0.15, 0));
 
 std::vector<tf::Vector3> gear_primary_loc;
-gear_primary_loc.push_back(tf::Vector3(-0.24, -0.24, 0));
-gear_primary_loc.push_back(tf::Vector3(0.24, -0.24, 0));
-gear_primary_loc.push_back(tf::Vector3(-0.24, 0.24, 0));
-gear_primary_loc.push_back(tf::Vector3(0.24, 0.24, 0));
-gear_primary_loc.push_back(tf::Vector3(-0.1693, -0.1693, 0));
-gear_primary_loc.push_back(tf::Vector3(0.1693, -0.1693, 0));
-gear_primary_loc.push_back(tf::Vector3(-0.1693, 0.1693, 0));
-gear_primary_loc.push_back(tf::Vector3(0.1693, 0.1693, 0));
-gear_primary_loc.push_back(tf::Vector3(-0.0986, -0.0986, 0));
-gear_primary_loc.push_back(tf::Vector3(0.0986, -0.0986, 0));
-gear_primary_loc.push_back(tf::Vector3(-0.0986, 0.0986, 0));
-gear_primary_loc.push_back(tf::Vector3(0.0986, 0.0986, 0));
+gear_primary_loc.push_back(tf::Vector3(-0.2, -0.2, 0));
+gear_primary_loc.push_back(tf::Vector3(-0.2, 0.2, 0));
+gear_primary_loc.push_back(tf::Vector3(0.2, -0.2, 0));
+gear_primary_loc.push_back(tf::Vector3(0.2, 0.2, 0));
+gear_primary_loc.push_back(tf::Vector3(-0.12, -0.12, 0));
+gear_primary_loc.push_back(tf::Vector3(-0.12, 0.12, 0));
+gear_primary_loc.push_back(tf::Vector3(0.12, -0.12, 0));
+gear_primary_loc.push_back(tf::Vector3(0.12, 0.12, 0));
+
 
 std::vector<tf::Vector3> pulley_primary_loc;
 pulley_primary_loc.push_back(tf::Vector3(-0.15, -0.15, 0));
@@ -37,15 +30,11 @@ pulley_primary_loc.push_back(tf::Vector3(-0.15, 0.15, 0));
 pulley_primary_loc.push_back(tf::Vector3(0.15, 0.15, 0));
 
 std::vector<tf::Vector3> piston_primary_loc;
-piston_primary_loc.push_back(tf::Vector3(-0.2377, -0.2377, 0));
-piston_primary_loc.push_back(tf::Vector3(0.2377, -0.2377, 0));
-piston_primary_loc.push_back(tf::Vector3(-0.2377, 0.2377, 0));
-piston_primary_loc.push_back(tf::Vector3(0.2377, 0.2377, 0));
-piston_primary_loc.push_back(tf::Vector3(-0.167, -0.167, 0));
-piston_primary_loc.push_back(tf::Vector3(0.167, -0.167, 0));
-piston_primary_loc.push_back(tf::Vector3(-0.167, 0.167, 0));
-piston_primary_loc.push_back(tf::Vector3(0.167, 0.167, 0));
-piston_primary_loc.push_back(tf::Vector3(-0.0963, -0.0963, 0));
-piston_primary_loc.push_back(tf::Vector3(0.0963, -0.0963, 0));
-piston_primary_loc.push_back(tf::Vector3(-0.0963, 0.0963, 0));
-piston_primary_loc.push_back(tf::Vector3(0.0963, 0.0963, 0));
+piston_primary_loc.push_back(tf::Vector3(-0.1962, -0.1962, 0));
+piston_primary_loc.push_back(tf::Vector3(-0.1962, 0.1962, 0));
+piston_primary_loc.push_back(tf::Vector3(0.1962, -0.1962, 0));
+piston_primary_loc.push_back(tf::Vector3(0.1962, 0.1962, 0));
+piston_primary_loc.push_back(tf::Vector3(-0.1132, -0.1132, 0));
+piston_primary_loc.push_back(tf::Vector3(-0.1132, 0.1132, 0));
+piston_primary_loc.push_back(tf::Vector3(0.1132, -0.1132, 0));
+piston_primary_loc.push_back(tf::Vector3(0.1132, 0.1132, 0));
